0654bc0cc8c23ce29406b6a460f0bead0900f151
Abir-a-creator <72654344+Abir-a-creator@users.noreply.github.com>
2021-03-13 17:27:21 +0100

    Update MovieList.js

--- movies/src/Components/MovieList.js
+++ movies/src/Components/MovieList.js
@@ -1,25 +1,21 @@
 import React from 'react'
-import Moviecard from './Moviecard';
+import MovieCard from './MovieCard'
 import AddMovie from './AddMovie'
-export default function MovieList({movie , name , setName  , ratingSearch , setRatingSearch}) {
- 
- 
+
+const MovieList = (props) => {
+
+    const { film } = props
+    const {addMovie}=props
+
     return (
-        <div className="cards">
-      {
-        movie.filter(
-          el=> 
-          el.name.toLowerCase().includes(name.toLowerCase().trim()) &&
-          el.rate >= ratingSearch
-        ).map((film , index)=>(
-            <Moviecard  key={index}  film ={film}/>
-            
-          )
-        )
-      
-      }
-      
+
+        <div>
+            <MovieCard author={film} movie={props.movie} />
+            <AddMovie addMovie={addMovie} />
         </div>
+
     )
-   
 }
+
+export default MovieList
+
